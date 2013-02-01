90af71d172e6d9dc0d019bc29b99bb2f8653ce7f
guzhengxiao <guzhengxiao@gmail.com>
2013-02-01 14:30:13 +0800

    init

---
+++ src/bshadow_app.erl
@@ -0,0 +1,16 @@
+-module(bshadow_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    bshadow_sup:start_link().
+
+stop(_State) ->
+    ok.
